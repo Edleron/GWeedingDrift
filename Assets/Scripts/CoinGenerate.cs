f9281819d33c2ab0ca69c16b8f589ca818b615db
Edleron <Edleron@hotmail.com>
2024-01-27 08:59:06 +0300

    fix : coin generate time features

--- Assets/Scripts/CoinGenerate.cs
+++ Assets/Scripts/CoinGenerate.cs
@@ -6,20 +6,23 @@ public class CoinGenerate : MonoBehaviour
 {
     public float borderX = 4;
     public float borderY = 2;
+    private float timer = 0;
 
     void Start()
     {
-        Invoke("CoinActiveted", 1.0f);
+        for (var i = 0; i < 10; i++)
+        {
+            timer += 0.25f;
+            Invoke("CoinActiveted", timer);
+        }       
        
     }
 
     // Update is called once per frame
     private void CoinActiveted()
     {
-        for (var i = 0; i < 10; i++)
-        {
-            Vector2 position = new Vector2(Random.Range(borderX, -borderX), Random.Range(borderY, -borderY));
-            GameObject coin = ObjectPooler.Instance.SpawnFromPool("Coin", position, Quaternion.identity);
-        }
+
+        Vector2 position = new Vector2(Random.Range(borderX, -borderX), Random.Range(borderY, -borderY));
+        GameObject coin = ObjectPooler.Instance.SpawnFromPool("Coin", position, Quaternion.identity);
     }
 }
